4c42704036dd140e6edc3386268f619732836014
Guilherme-Bodart <guilhermebodart73@gmail.com>
2019-07-04 16:18:12 -0300

    Ta quase pronto
    Quase

--- Codigo Fonte/projeto-jogo-frontend/src/views/CadastroDados.jsx
+++ Codigo Fonte/projeto-jogo-frontend/src/views/CadastroDados.jsx
@@ -6,18 +6,44 @@ import firebase from 'firebase'
 import userPadrao from '../assets/img/default-avatar.png'
 
 class CadastroDados extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
     this.state = {
       email: '',
       password: '',
       nascimento: '',
       message: '',
-      userId: 0,
       imageUrl: userPadrao,
+      titulo: 'Pessoais',
+      pagina: 0,
+      checkM:true,
+      checkF:false,
+      genero:true,
+      nickname:'',
     }
+    this.handleChangeF = this.handleChangeF.bind(this)
+    this.handleChangeM = this.handleChangeM.bind(this)
     this.onChangeInput = this.onChangeInput.bind(this)
   }
+
+  handleChangeM() {
+    this.setState({
+      checkM: true,
+      checkF: false,
+      genero: true
+
+    })
+  }
+
+  handleChangeF() {
+    this.setState({
+      checkF: true,
+      checkM: false,
+      genero: false
+
+    })
+  }
+
   onChangeInput(field, e) {
     this.setState({
       [field]: e.target.value
@@ -26,38 +52,42 @@ class CadastroDados extends Component {
   }
 
   tryAvance() {
-    console.log(this.state.email)
-    const { email, password, nascimento, imageUrl } = this.state
-    this.tryCreate(email, password,nascimento)
-      .then(() => this.writeUserData(email, nascimento, imageUrl)
-        .then(() => this.props.history.push("cadastroJogador")))
-
+    this.setState({      
+      pagina:1
+    })
   }
 
-  tryCreate(email, password,nascimento) {
-    if(nascimento !== ""){
+  tryVoltar(){
+    this.setState({      
+      pagina:0
+    })
+  }
+  
+  tryCreate(email,password,nascimento,imageUrl,nickname,genero) {
+
+    if(this.state.nascimento){
+      
       return firebase.auth().createUserWithEmailAndPassword(email, password)
-        .catch(function (error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          if (errorCode === 'auth/weak-password') {
-            alert('The password is too weak.');
-          } else {
-            alert(errorMessage);
-          }
-          console.log(error);
-          throw error
-        });
+        .then(response=>{
+          
+          this.tryCadastro(email,nascimento,imageUrl,nickname,genero)
+            
+          return response  
+        })
     }
+
+    throw "Erro ao tentar criar registro!";
   }
 
-  writeUserData(email, nascimento, imageUrl) {
+  tryCadastro(email, nascimento, imageUrl,nickname,genero) {
+    
     let db = firebase.firestore()
     return db.collection("Usuarios").doc().set({
       email: email,
       nascimento: nascimento,
-      imageUrl: imageUrl
+      imageUrl: imageUrl,
+      genero: genero===true ? 'Masculino' : 'Feminino',
+      nickname: nickname
     })
       .then(function (docRef) {
         console.log("Document written with ID: ", docRef.id);
@@ -69,69 +99,156 @@ class CadastroDados extends Component {
   }
 
   render() {
-    const database = firebase.firestore()
-    const { email, password, nascimento, userId, imageUrl } = this.state
-    return (
-      <div className="content">
-        <Container fluid>
-          <Form>
-            <Form.Label><p className="corBranca">Dados Pessoais</p></Form.Label>
-            <br />
-            <br />
-            <Form.Group>
-              <Form.Row>
-                <Col md={1}><p className="corBranca">Email</p></Col>
-                <Col md={{ span: 6, offset: 2 }}>
-                  <Form.Control type="email" placeholder="E-mail"
-                    value={email}
-                    onChange={value => this.onChangeInput('email', value)} />
-                </Col>
-              </Form.Row>
-            </Form.Group>
+    const { email, password, nascimento, imageUrl,titulo,pagina,genero,nickname,checkM,checkF } = this.state
+      switch(pagina){
+        case 0:
+          return(
+            <div className="content">
+            <Container fluid>
+              <Form onSubmit={(evt) => evt.preventDefault()}>
+                <Form.Label><p className="corBranca">Dados {titulo}</p></Form.Label>
+                <br />
+                <br />
+                <Form.Group>
+                  <Form.Row>
+                    <Col md={1}><p className="corBranca">Email</p></Col>
+                    <Col md={{ span: 6, offset: 2 }}>
+                      <Form.Control type="email" placeholder="E-mail"
+                        value={email}
+                        onChange={value => this.onChangeInput('email', value)} />
+                    </Col>
+                  </Form.Row>
+                </Form.Group>
 
-            <br />
-            <Form.Group>
-              <Form.Row>
-                <Col md={1}><p className="corBranca">Senha</p></Col>
-                <Col md={{ span: 6, offset: 2 }}>
-                  <Form.Control type="password" placeholder="Senha"
-                    value={password}
-                    onChange={value => this.onChangeInput('password', value)} />
-                </Col>
-              </Form.Row>
-            </Form.Group>
-            <br />
+                <br />
+                <Form.Group>
+                  <Form.Row>
+                    <Col md={1}><p className="corBranca">Senha</p></Col>
+                    <Col md={{ span: 6, offset: 2 }}>
+                      <Form.Control type="password" placeholder="Senha"
+                        value={password}
+                        onChange={value => this.onChangeInput('password', value)} />
+                    </Col>
+                  </Form.Row>
+                </Form.Group>
+                <br />
 
-            <Form.Row>
-              <Col md={1}><p className="corBranca">Nascimento</p></Col>
-              <Col md={{ span: 3, offset: 2 }}>
-                <Form.Control type="date" placeholder="Nascimento"
-                  value={nascimento}
-                  onChange={value => this.onChangeInput('nascimento', value)} />
-              </Col>
-            </Form.Row>
-            <br />
-            <Row>
-              <Col md={{ offset: 2 }}>
-                <NavLink to={"/admin/login"}>
-                  <Button variant="primary" type="submit">
-                    Voltar
-              </Button>
-                </NavLink>
-              </Col>
-              <Col md={{ offset: 2 }}>
-                <Button variant="primary" type="submit"
-                  onClick={() => this.tryAvance()}>
-                  Avançar
-                </Button>
-              </Col>
-            </Row>
-          </Form>
+                <Form.Row>
+                  <Col md={1}><p className="corBranca">Nascimento</p></Col>
+                  <Col md={{ span: 3, offset: 2 }}>
+                    <Form.Control type="date" placeholder="Nascimento"
+                      value={nascimento}
+                      onChange={value => this.onChangeInput('nascimento', value)} />
+                  </Col>
+                </Form.Row>
+                <br />
+                <Row>
+                  <Col md={{ offset: 2 }}>
+                    <NavLink to={"/admin/login"}>
+                      <Button variant="primary" type="submit">
+                        Voltar
+                  </Button>
+                    </NavLink>
+                  </Col>
+                  <Col md={{ offset: 2 }}>
+                    <Button variant="primary" type="submit"
+                      onClick={() => this.tryAvance()}>
+                      Avançar
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
 
-        </Container>
-      </div>
-    );
+            </Container>
+          </div>
+        )
+
+        case 1:
+          return(
+            <div className="content">
+            <Container fluid>
+              <Form onSubmit={(evt) => evt.preventDefault()}>
+                <Form.Label><p className="corBranca">Dados Jogador</p></Form.Label>
+                <br />
+                <br />
+                <Form.Row>
+                  <Col md={1}><p className="corBranca">Nickname</p></Col>
+                  <Col md={{ span: 6, offset: 2 }}>
+                    <Form.Control type="Login" placeholder="Nickname" 
+                    value={nickname} onChange={value => this.onChangeInput('nickname', value)} />/>
+                  </Col>
+                </Form.Row>
+                <br />
+                <Form.Row>
+                  <Col md={1}><p className="corBranca">Gênero</p></Col>
+                  <Col md={{ span: 4, offset: 2 }}>
+                      {/*Peguei Código +- Pronto */}
+                      {['checkbox'].map(type => (
+                        <div key={`custom-inline-${type}`} className="mb-3">
+                          <Form.Row>
+                            <Col md={{ span: 4, offset: 2 }}>
+                              <Form.Check
+                                checked={checkM}
+                                custom
+                                inline
+                                label
+                                type={type}
+                                id={`custom-inline-${type}-1`}
+                                onChange={this.handleChangeM} />
+                              <i size="lg" className={'fa fa-mars fa-3x'} ></i>
+                            </Col>
+    
+                            <br />
+                            <Col md={{ span: 4, offset: 2 }}>
+                              <Form.Check
+                                checked={checkF}
+                                custom
+                                inline
+                                label
+                                type={type}
+                                id={`custom-inline-${type}-2`}
+                                onChange={this.handleChangeF}
+                              /><i className={'fa fa-venus fa-3x'}></i>
+                            </Col>
+                          </Form.Row>
+                          <Form.Row>
+                            <Col md={{ span: 1, offset: 3 }}>
+                              <p className="corBranca">Masculino</p>
+                            </Col>
+                            <Col md={{ span: 1, offset: 5 }}>
+                              <p className="corBranca">Feminino</p>
+                            </Col>
+                          </Form.Row>
+                        </div>
+                      ))}
+                      {/* Até Aqui */}
+                  </Col>
+                </Form.Row>
+                <br />
+                <Row>
+                  <Col md={{ offset: 2 }}>
+                      <Button variant="primary" type="submit"
+                        onClick={()=>this.tryVoltar()}>
+                        Voltar
+                      </Button>
+                  </Col>
+                  <Col md={{ offset: 2 }}>
+                    <Button variant="primary" type="submit"
+                      onClick={() => this.tryCreate(email,password, nascimento, imageUrl,nickname,genero)}>
+                      Cadastrar
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Container>
+          </div >
+        );
+      }
+
+
+      
   }
+
 }
 
 export default CadastroDados;
